95a7b76cdb040e261452a83a8728b6d71f350c43
Priyanka Chatterjee <91906722+dollpriyanka@users.noreply.github.com>
2023-06-17 18:12:12 +0000

    add: uncommitted change

--- my-app/src/components/sideBar/sidebar.js
+++ my-app/src/components/sideBar/sidebar.js
@@ -9,6 +9,7 @@ import members from "../assest/members.svg";
 import settings from "../assest/settings.svg";
 import addSquare from "../assest/addSquare.svg";
 import dot from "../assest/dot.svg";
+import lamp from "../assest/lamp.svg";
 
 
 
@@ -59,9 +60,9 @@ function Sidebar() {
                     </div>
                 </div>
                 <div className="section2_items active">
-                        <div className="dot green"></div>
-                        <p className="item">Mobile App</p>
-                        <img src={dot}></img>
+                    <div className="dot green"></div>
+                    <p className="item">Mobile App</p>
+                    <img src={dot}></img>
                 </div>
                 <div className="section2_items">
                     <div className="dot orange"></div>
@@ -74,6 +75,21 @@ function Sidebar() {
                 <div className="section2_items">
                     <div className="dot blue"></div>
                     <p className="item">Wireframes</p>
+                </div>
+            </div>
+
+            <div className="sidebar_section3">
+                <div className="section3_image">
+                    <img src={lamp}></img>
+                </div>
+                <div className="section3_header">
+                    Thoughts Time
+                </div>
+                <div className="section3_body">
+                    We don’t have any notice for you, till then you can share your thoughts with your peers.
+                </div>
+                <div className="section3_button">
+                    <button className="button">Write a message</button>
                 </div>
             </div>
 
