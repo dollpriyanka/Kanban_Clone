ee0938faea31b2e85a4e43d2ca012fa7c12230d3
Priyanka Chatterjee <91906722+dollpriyanka@users.noreply.github.com>
2023-06-18 06:58:50 +0000

    add: header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import react from 'react';
 import Sidebar from './components/sideBar/sidebar.js';
+import "./App.css";
+import Header from './components/header/header.js';
+
 
 function App() {
   return (
-    <div>
+    <div className='sidebar'>
       <Sidebar />
+      <Header />
     </div>
   );
 }
---
+++ my-app/src/components/header/header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import './header.css';
+import searchbar from '../assest/searchbar.svg';
+import header_icon1 from '../assest/header_icon1.svg';
+import header_icon2 from '../assest/header_icon2.svg';
+import header_icon3 from '../assest/header_icon3.svg';
+import dropdown from '../assest/dropdown.svg';
+import header_avatar from '../assest/header_avatar.svg';
+
+
+
+
+
+function Header() {
+    return (
+        <div className="main">
+            <div className="search">
+                <img src={searchbar} className="searchbar_image"></img>
+                <input text="Search" placeholder="Search for anything..." className="search_input" />
+            </div>
+            <div className="icons">
+                <img src={header_icon1} className="header_icons"></img>
+                <img src={header_icon2} className="header_icons"></img>
+                <img src={header_icon3} className="header_icons"></img>
+                <div className="avatar">
+                    <div className="name">Anima<br /> Agarwal</div>
+                    <img src={header_avatar}></img>
+                    <img src={dropdown}></img>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Header;
